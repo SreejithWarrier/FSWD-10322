71f878f3016a66a1557a04e20c8b329d7a7539f8
SreejithWarrier <shreejithwarrier711@gmail.com>
2022-05-07 20:21:00 +0530

    Objects in js

--- app.js
+++ app.js
@@ -9,18 +9,30 @@
 // console.info('Here i am');
 // console.log(window);
 // window.alert('this is a alert')
-const obj = {
-    name:"C class",
-    brand: "Mercedes",
-    model:2022,
-    log:function(){
-        console.log(this)
-    }
-}
-console.log(this);
-obj.log();
+// const obj = {
+//     name:"C class",
+//     brand: "Mercedes",
+//     model:2022,
+//     log:function(){
+//         console.log(this)
+//     }
+// }
+// console.log(this);
+// obj.log();
 
-function greet(){
-    console.log('hi');
+// function greet(){
+//     console.log('hi');
+// }
+// greet();
+
+const Car = function(name,brand,model){
+    console.log(this)
+    this.name = name
+    this.brand = brand
+    this.model = model
 }
-greet();
+
+const car1 = new Car('C 200D','Mercedes',2022)
+const car2 = new Car('M2 Comp','BMW',2022)
+
+console.log(car1)
