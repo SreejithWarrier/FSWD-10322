8c697f6eb480af726dc58ec165806f76c5ae41ac
SreejithWarrier <shreejithwarrier711@gmail.com>
2022-06-06 20:10:51 +0530

    Server

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("hello World")
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const server = http.createServer((req,res)=>{
+//     //   console.log(req.headers)
+//     //   console.log(res)
+//     //   console.log(req.headers)
+     
+//     // res.write("<h1>PING from server</h1>")
+//     res.write("<h1>This is a Response</h1>")
+//     res.end();
+// })
+if(req.url=='/home'){
+    const data = fs.readFileSync(path.join(__dirname,'/public/home.html'))
+    res.write(data)
+}
+else if (req.url=='/about')
+{
+        const data = fs.readFileSync(path.join(__dirname,'/public/about.html'))
+     res.write(data)
+
+    }
+    res.end();
+
+}
+)
+
+
+server.listen(8080);
